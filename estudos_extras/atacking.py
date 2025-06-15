663ec5f7b5598d12bd0afcd08a88c77f72bdba40
Fernando Augusto <nando.aju.2001@gmail.com>
2025-06-15 13:12:40 -0300

    Estudos Extras 2 - Cyber e Env

--- estudos_extras/atacking.py
+++ estudos_extras/atacking.py
@@ -7,3 +7,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     client_socket.connect((HOST, PORT))
     mensagem = client_socket.recv(1024)
     print(f"Mensagem recebida do servidor: {mensagem.decode()}")
+    
